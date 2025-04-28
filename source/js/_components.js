9e6370e75a1a6e650e9fffc344e64de7964a8a1d
Roman <romanshliakhov@gmail.com>
2025-04-28 12:16:36 +0300

    add text section

--- source/js/_components.js
+++ source/js/_components.js
@@ -4,14 +4,8 @@ import './components/acc.js';
 import './components/mobile-menu.js';
 import './components/tabs.js';
 import './components/select.js';
-import './components/forms.js';
-import './components/counter.js';
-import './components/filter.js';
-import './components/cards.js';
 import './components/sliders.js';
-import './components/search.js';
-import './components/hiddenText.js';
-
+// import './components/search.js';
 import './components/fancybox.js';
 
 
